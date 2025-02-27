cbed5179f751e24ec81ca601b9579ea4893fb1a6
Yoel García Lago <ygarcialago@danielcastelao.org>
2025-02-27 11:07:33 +0100

    Cliente en proceso de terminarse

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,5 +1,50 @@
-public class Cliente {
-    public static void main(String[]args){
+import java.io.*;
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class Cliente {
+
+    private String nombreUsuario;
+    private String ipServidor;
+    private int puerto;
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private MetodosCliente metodosCliente;
+
+    public Cliente(Socket socket, String nombreUsuario) {
+        this.socket = socket;
+        this.nombreUsuario = nombreUsuario;
+        this.metodosCliente = new MetodosCliente();
+        try {
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            metodosCliente.cerrarTodo(this.socket, this.bufferedWriter, this.bufferedReader);
+        }
     }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public BufferedReader getBufferedReader() {
+        return bufferedReader;
+    }
+
+    public BufferedWriter getBufferedWriter() {
+        return bufferedWriter;
+    }
+
+    public static void main(String[]args){
+    }
+
+
+
 }
