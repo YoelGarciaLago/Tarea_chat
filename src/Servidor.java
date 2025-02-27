c7758c9c398be9082590c00a85e0b51e8ae5de9f
Yoel García Lago <ygarcialago@danielcastelao.org>
2025-02-27 11:00:34 +0100

    Creación de la estructura base

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,5 +1,39 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Servidor {
     public static void main(String[]args){
+        ArrayList <Socket> listaDeUsuarios = new ArrayList<>();
+        ArrayList <String> listaMensajes = new ArrayList<>();
 
+        MetodosConexionServer metodosConexionServer = new MetodosConexionServer();
+        ServerSocket serverSocket = metodosConexionServer.crearServer();
+        metodosConexionServer.bindAlPuerto(6666,serverSocket);
+
+
+        ExecutorService hilosCliente = Executors.newFixedThreadPool(3);
+
+        while (true){
+            try {
+                if(serverSocket == null){
+                    throw new IOException();
+                }
+                Socket cliente = serverSocket.accept();
+                System.out.println("Nuevo cliente conectado");
+                ManejoCliente nuevoCliente = new ManejoCliente(cliente);
+                hilosCliente.execute(nuevoCliente);
+
+            } catch (IOException e) {
+                metodosConexionServer.closeServer(serverSocket);
+                break;
+            }
+        }
+        metodosConexionServer.closeServer(serverSocket);
     }
 }
