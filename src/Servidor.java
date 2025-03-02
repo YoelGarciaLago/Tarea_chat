b94296dded12e68ee85543ac7455f7ac8a724873
yoinormie <yoelgarcialago@gmail.com>
2025-03-02 15:52:18 +0100

    Métodos de acceso a la lista de mensajes + envío de historial al nuevoCliente

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,16 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Servidor {
+    private static ArrayList <String> listaMensajes = new ArrayList<>();
+
+    public static ArrayList<String> getListaMensajes() {
+        return listaMensajes;
+    }
+
+    public static void setListaMensajes(ArrayList<String> listaMensajes) {
+        Servidor.listaMensajes = listaMensajes;
+    }
+    public static void enviarHistorialMensajes(ArrayList<String> historialMensajes, ManejoCliente cliente) {
+        try {
+            BufferedWriter writer = cliente.getBufferedWriter();
+            for (String mensaje : historialMensajes) {
+                writer.write(mensaje);
+                writer.newLine();
+            }
+            writer.flush(); // Asegura que los mensajes se envíen
+        } catch (IOException e) {
+            MetodosCliente.cerrarTodo(cliente.getCliente(), cliente.getBufferedWriter(), cliente.getBufferedReader());
+        }
+    }
     public static void main(String[]args){
-        ArrayList <Socket> listaDeUsuarios = new ArrayList<>();
-        ArrayList <String> listaMensajes = new ArrayList<>();
 
         MetodosConexionServer metodosConexionServer = new MetodosConexionServer();
         ServerSocket serverSocket = metodosConexionServer.crearServer();
@@ -18,15 +38,15 @@ public class Servidor {
 
 
         ExecutorService hilosCliente = Executors.newFixedThreadPool(3);
-
         while (true){
             try {
-                if(serverSocket == null){
-                    throw new IOException();
-                }
                 Socket cliente = serverSocket.accept();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+                String nombreUsuario = reader.readLine(); // 🔴 Leer el nickname enviado por el cliente
+
                 System.out.println("Nuevo cliente conectado");
-                ManejoCliente nuevoCliente = new ManejoCliente(cliente);
+                ManejoCliente nuevoCliente = new ManejoCliente(cliente, nombreUsuario);
+                Servidor.enviarHistorialMensajes(Servidor.getListaMensajes(),nuevoCliente);
                 hilosCliente.execute(nuevoCliente);
 
             } catch (IOException e) {
@@ -36,4 +56,6 @@ public class Servidor {
         }
         metodosConexionServer.closeServer(serverSocket);
     }
+
+
 }
